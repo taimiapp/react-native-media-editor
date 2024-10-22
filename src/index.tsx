ac818b480c32528ff3d40ab6877c3f429554c3ba
artiehrsmv <artem.gerasimov@appflame.com>
2024-10-22 12:31:02 +0300

    TCropResult interface exported

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import hooks from './hooks';
 import components from './components';
-import type { TCropperRef, TMediaCropperProps } from './interfaces/interface';
+import type {
+  TCropperRef,
+  TMediaCropperProps,
+  TCropResult,
+} from './interfaces/interface';
 
 export const useVideoEditorState = hooks.useVideoEditorState;
 export const MediaCropper = components.MediaCropper;
 
-export type { TCropperRef, TMediaCropperProps };
+export type { TCropperRef, TMediaCropperProps, TCropResult };
 
 export default { useVideoEditorState, MediaCropper };
