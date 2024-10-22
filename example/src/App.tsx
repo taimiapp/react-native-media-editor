426eb1ade657b59d8c91aba94ffb71dec0c51108
artiehrsmv <artem.gerasimov@appflame.com>
2024-10-22 14:18:32 +0300

    example updated

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return null;
-}
+import { SafeAreaView, Text } from 'react-native';
+import { MediaCropper, TCropResult } from 'react-native-media-editor';
+
+const App = (): JSX.Element => {
+  const handleCropAreaChange = (_data: TCropResult) => {
+    // @TODO implement
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>hello world</Text>
+      <MediaCropper onCropAreaChange={handleCropAreaChange} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
