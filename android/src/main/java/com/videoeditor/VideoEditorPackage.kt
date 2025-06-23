e02c4b4c76f038e0ca5b5d1e3a435a4851a9986c
artiehrsmv <artem.gerasimov@appflame.com>
2025-06-23 16:57:26 +0300

    imports for android fixed

--- android/src/main/java/com/videoeditor/VideoEditorPackage.kt
+++ android/src/main/java/com/videoeditor/VideoEditorPackage.kt
@@ -4,7 +4,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.videoeditor.VideoEditorModule
+import com.videoEditor.VideoEditorModule
 
 class VideoEditorPackage: ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
